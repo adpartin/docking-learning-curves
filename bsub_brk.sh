85a95719d4e560e58bd2b69b1a457213df169774
Alex Partin <25892264+adpartin@users.noreply.github.com>
2020-05-11 20:34:04 -0400

    New bash script to process subsets
    Add new bash scripts that process subsets of training subsets.

--- bsub_brk.sh
+++ bsub_brk.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #BSUB -P med106
-#BSUB -W 00:10
+#BSUB -W 06:00
 #BSUB -nnodes 50
 #BSUB -J my-bsub
 # ----------------------------------------------
@@ -18,7 +18,8 @@ echo "Bash version ${BASH_VERSION}"
 # N_SPLITS=$(($NODES * $GPU_PER_NODE))
 
 # Set resources based on number of splits
-# N_SPLITS=100
+N_SPLITS=100
+START_SPLIT=1
 # PARTS=3
 # GPUs=$(($N_SPLITS * $PARTS))
 # NODES=$(($GPUs / $GPU_PER_NODE))
@@ -33,53 +34,31 @@ GLOBAL_SUFX="bsub_3parts"
 # echo "Number of data splits for LC: $N_SPLITS"
 # echo "Global outdir dir name: $GLOBAL_SUFX"
 
-# gpu_counter=0
-# node_counter=0
-# device=0
-# for split in $(seq 0 1 $N_SPLITS); do
-#     if [[ $device -lt 6 ]]; then
-#         jsrun -n 1 -a 1 -c 4 -g 1 ./jsrun_b1.sh $device $id $GLOBAL_SUFX exec >run"$id".log 2>&1 &
-#     else
-#         device=0
-#         jsrun -n 1 -a 1 -c 4 -g 1 ./jsrun_b1.sh $device $id $GLOBAL_SUFX exec >run"$id".log 2>&1 &
-#     fi
-#     device=$(($device + 1))
-
-# gpu_counter=0
-# node_counter=0
-# device=-1
-# for split in $(seq 0 1 $N_SPLITS); do
-#     device=$(($device + 1))
-#     if [[ $device -eq 6 ]]; then
-#         device=0
-#     fi
-#     echo "Device $device"
-#     # jsrun -n 1 -a 1 -c 4 -g 1 ./jsrun_b1.sh $device $id $GLOBAL_SUFX exec >run"$id".log 2>&1 &
-# done
-
 SET=1
 out_dir=$GLOBAL_SUFX/$SET
 echo "Dir $out_dir"
-for split in $(seq 0 1 $N_SPLITS); do
+for split in $(seq $START_SPLIT 1 $N_SPLITS); do
     device=$(($split % 6))
-    jsrun -n 1 -a 1 -c 4 -g 1 ./jsrun_brk.sh $device $split $out_dir $SET exec >run"$split".log 2>&1 &
+    echo "Set $SET; Split $split; Device $device"
+    jsrun -n 1 -a 1 -c 4 -g 1 ./jsrun_brk.sh $device $split $out_dir $SET exec >logs/run"$split".log 2>&1 &
 done
 
 SET=2
 out_dir=$GLOBAL_SUFX/$SET
 echo "Dir $out_dir"
-for split in $(seq 0 1 $N_SPLITS); do
+for split in $(seq $START_SPLIT 1 $N_SPLITS); do
     device=$(($split % 6))
-    echo "Dir $out_dir"
-    jsrun -n 1 -a 1 -c 4 -g 1 ./jsrun_brk.sh $device $split $out_dir $SET exec >run"$split".log 2>&1 &
+    echo "Set $SET; Split $split; Device $device"
+    jsrun -n 1 -a 1 -c 4 -g 1 ./jsrun_brk.sh $device $split $out_dir $SET exec >logs/run"$split".log 2>&1 &
 done
 
 SET=3
 out_dir=$GLOBAL_SUFX/$SET
 echo "Dir $out_dir"
-for split in $(seq 0 1 $N_SPLITS); do
+for split in $(seq $START_SPLIT 1 $N_SPLITS); do
     device=$(($split % 6))
-    jsrun -n 1 -a 1 -c 4 -g 1 ./jsrun_brk.sh $device $split $out_dir $SET exec >run"$split".log 2>&1 &
+    echo "Set $SET; Split $split; Device $device"
+    jsrun -n 1 -a 1 -c 4 -g 1 ./jsrun_brk.sh $device $split $out_dir $SET exec >logs/run"$split".log 2>&1 &
 done
 
 
--- jsrun_brk.sh
+++ jsrun_brk.sh
@@ -13,38 +13,57 @@ id=$2
 global_sufx=$3
 SET=$4
 
-gout="/gpfs/alpine/med106/scratch/$USER/$global_sufx"
+# DATAPATH="data/sample.data.parquet"
+# DATAPATH="data/ml.ADRP.parquet"
+
+DATAPATH="data/ml.3CLPro_pocket1_dock.parquet"
+SPLITPATH="data/ml.3CLPro_pocket1_dock.splits"
+TARGET="3CLPro_pocket1"
+
+# DATAPATH="data/ml.ADRP-ADPR_pocket1_dock.parquet"
+# SPLITPATH="data/ml.ADRP-ADPR_pocket1_dock.splits"
+# TARGET="ADRP-ADPR_pocket1"
+
+gout="/gpfs/alpine/med106/scratch/$USER/$global_sufx/$TARGET"
 mkdir -p $gout
 
 export CUDA_VISIBLE_DEVICES=$device
 
-# DATAPATH="data/sample.data.parquet"
-DATAPATH="data/ml.ADRP.parquet"
-EPOCH=2
+# EPOCH=2
+# EPOCH=100
 # EPOCH=350
+EPOCH=500
 
 echo "Using cuda device $device"
 echo "CUDA_VISIBLE_DEVICES: $CUDA_VISIBLE_DEVICES"
 echo "Run id: $id"
 echo "Global output: $gout"
+echo "Target name: $TARGET"
 
 # [128 329 845 2174 5589 14368 36937 94952 244088]
+#  1   2   9
+# [128 329 244088]
+#  3   4    8 
+# [845 2174 94952]
+#  5    6     7 
+# [5589 14368 36937]
+
 # [128 2174 36937]
 # [329 5589 94952]
 # [845 14368 244088]
 
 if [[ $SET -eq 1 ]]; then
-    echo "Set $SET"
-    python src/main_lc.py -dp $DATAPATH --lc_sizes_arr 128 2174 36937 --epoch $EPOCH \
-        --gout $gout --rout run"$id" -sc stnd > "$gout"/run"$id".log 2>&1
+    echo "LC subset set $SET"
+    python src/main_lc.py -dp $DATAPATH -sd $SPLITPATH --split_id $id --lc_sizes_arr 128 329 244088 \
+        --epoch $EPOCH --gout $gout --rout run"$id" -sc stnd --ml keras > "$gout"/run"$id".log 2>&1
 elif [[ $SET -eq 2 ]]; then
-    echo "Set $SET"
-    python src/main_lc.py -dp $DATAPATH --lc_sizes_arr 329 5589 94952 --epoch $EPOCH \
-        --gout $gout --rout run"$id" -sc stnd > "$gout"/run"$id".log 2>&1
+    echo "LC subset set $SET"
+    python src/main_lc.py -dp $DATAPATH -sd $SPLITPATH --split_id $id --lc_sizes_arr 845 2174 94952 \
+        --epoch $EPOCH --gout $gout --rout run"$id" -sc stnd --ml keras > "$gout"/run"$id".log 2>&1
 elif [[ $SET -eq 3 ]]; then
-    echo "Set $SET"
-    python src/main_lc.py -dp $DATAPATH --lc_sizes_arr 845 14368 244088 --epoch $EPOCH \
-        --gout $gout --rout run"$id" -sc stnd > "$gout"/run"$id".log 2>&1
+    echo "LC subset set $SET"
+    python src/main_lc.py -dp $DATAPATH -sd $SPLITPATH --split_id $id --lc_sizes_arr 5589 14368 36937 \
+        --epoch $EPOCH --gout $gout --rout run"$id" -sc stnd --ml keras > "$gout"/run"$id".log 2>&1
 fi
 
 
